b9be7ce1454f43f6760099fa250572c774c77989
gotowskilukasz@gmail.com <gotowskilukasz@gmail.com>
2023-04-12 21:07:13 +0200

    kolejny myslnik

--- skrypt.sh
+++ skrypt.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 if [[ $d == "--date" || $d == "-d" ]]; then
-data="$(date)"
-echo $data
+    data="$(date)"
+    echo $data
 
 elif [[ $d == "--logs" || $d == "-l" ]]; then
-for ((i=1; i<=100; i++)); do
-      fn="log$i.txt"
-      echo -e "Nazwa pliku to : $fn\nNazwa skryptu to: $0\nData: $(date)" > $fn
-    done
+    for ((i=1; i<=100; i++)); do
+        fn="log$i.txt"
+        echo -e "Nazwa pliku to : $fn\nNazwa skryptu to: $0\nData: $(date)" > $fn
+        done
 
-for ((i=1; i<=$liczba; i++)); do
-      fn="log$i.txt"
-      echo -e "Nazwa pliku to : $fn\nNazwa skryptu to: $0\nData: $(date)" > $fn
-    done
+    for ((i=1; i<=$liczba; i++)); do
+        fn="log$i.txt"
+        echo -e "Nazwa pliku to : $fn\nNazwa skryptu to: $0\nData: $(date)" > $fn
+        done
 
 elif [[ $d == "--help" || $d == "-h" ]]; then
-echo "Opcje : "
-echo " -d  --date   Wyświetla aktualną datę"
-echo " -h  --help   Wyświetla pomoc"
-echo " -l  --logs   Tworzy x plików logx.txt, [nazwa pliku --logs x], x - liczba plików"
+    echo "Opcje : "
+    echo " -d  --date   Wyświetla aktualną datę"
+    echo " -h  --help   Wyświetla pomoc"
+    echo " -l  --logs   Tworzy x plików logx.txt, [nazwa pliku --logs x], x - liczba plików"
+
+elif [[ $d == "--init" ]]; then
+    git clone https://github.com/LukaszGotowski/lab4.git
+    export PATH="$PATH:$(pwd)/lab4" 
