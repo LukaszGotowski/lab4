9f3fd08cd57612792078c336c4a9accd9d5651b6
gotowskilukasz@gmail.com <gotowskilukasz@gmail.com>
2023-04-12 20:00:33 +0200

    myslnik4

--- skrypt.sh
+++ skrypt.sh
@@ -2,7 +2,12 @@
 data="$(date)"
 echo $data
 
-for ((i=1; i<=$2; i++)); do
+for ((i=1; i<=100; i++)); do
+      fn="log$i.txt"
+      echo -e "Nazwa pliku to : $fn\nNazwa skryptu to: $0\nData: $(date)" > $fn
+    done
+
+for ((i=1; i<=$liczba; i++)); do
       fn="log$i.txt"
       echo -e "Nazwa pliku to : $fn\nNazwa skryptu to: $0\nData: $(date)" > $fn
     done
