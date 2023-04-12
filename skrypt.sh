69b461ccab4df26c3e3c9d79e434353619b74193
gotowskilukasz@gmail.com <gotowskilukasz@gmail.com>
2023-04-12 20:39:21 +0200

    myslnik5

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+if [[ $1 == "--date"]]; then
 data="$(date)"
 echo $data
 
+elif [[ $1 == "--logs"]]; then
 for ((i=1; i<=100; i++)); do
       fn="log$i.txt"
       echo -e "Nazwa pliku to : $fn\nNazwa skryptu to: $0\nData: $(date)" > $fn
@@ -11,3 +13,8 @@ for ((i=1; i<=$liczba; i++)); do
       fn="log$i.txt"
       echo -e "Nazwa pliku to : $fn\nNazwa skryptu to: $0\nData: $(date)" > $fn
     done
+
+elif [[ $1 == "--help"]]; then
+echo "Opcje : "
+echo " --date   Wyświetla aktualną datę"
+echo " --logs   Tworzy x plików logx.txt, [nazwa pliku --logs x], x - liczba plików"
